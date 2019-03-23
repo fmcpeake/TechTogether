40efc9672cc18ce438208d044ef977743471e8f8
Stella1017 <list.stella@gmail.com>
2019-03-23 18:52:41 -0400

    shiny app

---
+++ BPS-Crime/server.R
@@ -0,0 +1,172 @@
+library(shiny)
+library(sp)
+library(sf)
+library(mapview)
+library(tidyverse)
+library(plotly)
+library(png)
+library(stringi)
+
+load("finaldata.Rdata")
+
+shinyServer(function(input, output) {
+  
+  pop <- finaldata %>%
+    mutate('School Size' = School_Total,
+           'School Name' = school_name_2,
+           Address = ADDRESS) %>%
+    select('School Name', Address, 'School Size')
+  
+  coordinates(finaldata) <- ~ X + Y
+  proj4string(finaldata) <- "+init=epsg:4326"
+  # mapview(finaldata, zcol="SCH_TYPE", legend=TRUE, cex=8,
+  #         popup=popupTable(pop)) 
+  
+  output$leaflet <- renderMapview(finaldata, zcol="SCH_TYPE", legend=TRUE, cex=8,
+                                  popup=popupTable(pop)) 
+  
+  clicked_leaflet <- reactiveValues(clickedMarker=NULL)
+  observeEvent(input$leaflet_marker_click,{
+    clicked_leaflet$clickedMarker <- input$leaflet_marker_click
+  })
+  
+  
+  selected_coordinates= reactive(({
+    c(clicked_leaflet$clickedMarker$lng,clicked_leaflet$clickedMarker$lat)
+  }))
+  
+  output$fish=renderTable({
+    selected_data()
+  })
+  
+  selected_data= reactive(({
+    if(is.null(clicked_leaflet$clickedMarker))
+      return(NULL)
+    
+    filter(collected_data, longitude == as.numeric(as.character(selected_coordinates()[1])),latitude==as.numeric(as.character(selected_coordinates()[2])))
+  }))
+  
+  output$max_min_temperature_plotly_16days=renderPlotly({
+    temp=selected_data()
+    if(is.null(temp))
+      return(NULL)
+    plot_ly() %>%
+      add_lines(x = temp$date, y = temp$max_temperature, name = "Maximum Temperature") %>%
+      add_lines(x = temp$date, y = temp$min_temperature, name = "Minimum Temperature")%>%
+      add_lines(x = temp$date, y = temp$day_temperature, name = "Day Temperature") %>%
+      add_lines(x = temp$date, y = temp$night_temperature, name = "Night Temperature") %>%
+      add_lines(x = temp$date, y = temp$eve_temperature, name = "Evening Temperature") %>%
+      add_lines(x = temp$date, y = temp$morn_temperature, name = "Morning Temperature") %>%
+      layout(title = '',
+             legend = list(orientation = 'h'),
+             xaxis = list(title = ""))
+  })
+  
+  output$max_min_temperature_plotly_3days=renderPlotly({
+    temp=selected_data()
+    if(is.null(temp))
+      return(NULL)
+    
+    temp=temp[1:3,]
+    plot_ly() %>%
+      add_lines(x = temp$date, y = temp$max_temperature, name = "Maximum Temperature") %>%
+      add_lines(x = temp$date, y = temp$min_temperature, name = "Minimum Temperature")%>%
+      add_lines(x = temp$date, y = temp$day_temperature, name = "Day Temperature") %>%
+      add_lines(x = temp$date, y = temp$night_temperature, name = "Night Temperature") %>%
+      add_lines(x = temp$date, y = temp$eve_temperature, name = "Evening Temperature") %>%
+      add_lines(x = temp$date, y = temp$morn_temperature, name = "Morning Temperature") %>%
+      layout(title = '',
+             legend = list(orientation = 'h'),
+             xaxis = list(title = ""))
+  })
+  
+  output$max_min_temperature_plotly_5days=renderPlotly({
+    temp=selected_data()
+    if(is.null(temp))
+      return(NULL)
+    temp=temp[1:5,]
+    plot_ly() %>%
+      add_lines(x = temp$date, y = temp$max_temperature, name = "Maximum Temperature") %>%
+      add_lines(x = temp$date, y = temp$min_temperature, name = "Minimum Temperature")%>%
+      add_lines(x = temp$date, y = temp$day_temperature, name = "Day Temperature") %>%
+      add_lines(x = temp$date, y = temp$night_temperature, name = "Night Temperature") %>%
+      add_lines(x = temp$date, y = temp$eve_temperature, name = "Evening Temperature") %>%
+      add_lines(x = temp$date, y = temp$morn_temperature, name = "Morning Temperature") %>%
+      layout(title = '',
+             legend = list(orientation = 'h'),
+             xaxis = list(title = ""))  
+  })
+  
+  
+  output$humidty_rain_cloudness_16days=renderPlotly({
+    temp=selected_data()
+    if(is.null(temp))
+      return(NULL)
+    
+    plot_ly(temp, x = ~date, y = ~clouds, type = 'bar', name = 'Clouds') %>%
+      add_trace(y = ~humidty, name = 'Humidity') %>%
+      layout(yaxis = list(title = '%'), barmode = 'group')%>%
+      add_trace(x = ~date, y = ~rain, type = 'scatter', mode = 'lines', name = 'Rainfall', yaxis = 'y2',
+                line = list(color = '#45171D'),
+                hoverinfo = "text",
+                text = ~paste(rain, '°F')) %>%
+      layout(title = '',
+             xaxis = list(title = ""),
+             yaxis = list(side = 'left', title = 'Humidity, clouds (%)', showgrid = FALSE, zeroline = FALSE),
+             yaxis2 = list(side = 'right', overlaying = "y", title = 'Rainfall', showgrid = FALSE, zeroline = FALSE))
+  })
+  
+  
+  output$humidty_rain_cloudness_5days=renderPlotly({
+    temp=selected_data()
+    if(is.null(temp))
+      return(NULL)
+    temp=temp[1:5,]
+    plot_ly(temp, x = ~date, y = ~clouds, type = 'bar', name = 'Clouds') %>%
+      add_trace(y = ~humidty, name = 'Humidity') %>%
+      layout(yaxis = list(title = '%'), barmode = 'group')%>%
+      add_trace(x = ~date, y = ~rain, type = 'scatter', mode = 'lines', name = 'Rainfall', yaxis = 'y2',
+                line = list(color = '#45171D'),
+                hoverinfo = "text",
+                text = ~paste(rain, '°F')) %>%
+      layout(title = '',
+             xaxis = list(title = ""),
+             yaxis = list(side = 'left', title = 'Humidity, clouds (%)', showgrid = FALSE, zeroline = FALSE),
+             yaxis2 = list(side = 'right', overlaying = "y", title = 'Rainfall', showgrid = FALSE, zeroline = FALSE))      
+  })
+  
+  
+  output$humidty_rain_cloudness_3days=renderPlotly({
+    temp=selected_data()
+    if(is.null(temp))
+      return(NULL)
+    temp=temp[1:3,]
+    plot_ly(temp, x = ~date, y = ~clouds, type = 'bar', name = 'Clouds') %>%
+      add_trace(y = ~humidty, name = 'Humidity') %>%
+      layout(yaxis = list(title = '%'), barmode = 'group')%>%
+      add_trace(x = ~date, y = ~rain, type = 'scatter', mode = 'lines', name = 'Rainfall', yaxis = 'y2',
+                line = list(color = '#45171D'),
+                hoverinfo = "text",
+                text = ~paste(rain, '°F')) %>%
+      layout(title = '',
+             xaxis = list(title = ""),
+             yaxis = list(side = 'left', title = 'Humidity, clouds (%)', showgrid = FALSE, zeroline = FALSE),
+             yaxis2 = list(side = 'right', overlaying = "y", title = 'Rainfall', showgrid = FALSE, zeroline = FALSE))     
+  })
+  
+  condition1<-reactive({
+    if(is.null(selected_data())){
+      result=0
+    }else{
+      result=1
+    }
+    result
+  })
+  
+  output$condition1 <- renderText({
+    condition1()
+  })
+  
+  outputOptions(output, 'condition1', suspendWhenHidden=FALSE)
+  
+})
---
+++ BPS-Crime/ui.R
@@ -0,0 +1,42 @@
+library(leaflet)
+library(plotly)
+shinyUI(fluidPage(
+  fluidRow(
+    column(width=6,align="center",
+           br(),
+           tags$h4(tags$strong("Leaflet, Plotly and Shiny: Weather Forecasts In The Northeast",
+                               style="text-align:center;color: #0000ff")),
+           
+           leafletOutput("leaflet",height = 500)
+    ),
+    
+    column(width=6,align="center",
+           conditionalPanel(
+             
+             condition = "output.condition1 == 0",
+             br(),
+             tags$h4("About",style='color:blue'),
+             tags$p('...',span(tags$a(href="https://openweathermap.org/forecast16", "OpenWeatherMap.")) ,style="text-align:left;color:black;font-size:140%")
+             ),
+           
+           
+           conditionalPanel(   
+             condition = "output.condition1 == 1",
+             tabsetPanel(
+               tabPanel(tags$em("Academic Performance",style="font-size:120%"),
+                        tags$hr(style="border-color: #ffc266;"),
+                        plotlyOutput("school_info")
+                          
+                        )),
+               
+               tabPanel(tags$em("Crime Around Schools",style="font-size:120%"),
+                        tags$hr(style="border-color:  #d27979;"),
+                        plotlyOutput("crime_around_schools")),
+               
+               tabPanel(tags$em("Science MCAS and Crime",style="font-size:120%"),
+                        tags$hr(style="border-color:  #d27979;"),
+                        plotlyOutput("science_and_crime")
+                        
+                        ))
+             )))
+)
